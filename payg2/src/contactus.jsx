1dcbc51819d4d06f04fdcf998da5d6ed801af434
AshishKumarSamantaray <121350516+AshishKumarSamantaray@users.noreply.github.com>
2024-08-26 00:09:18 +0530

    commit

--- payg2/src/contactus.jsx
+++ payg2/src/contactus.jsx
@@ -1,12 +1,21 @@
 import contactHeader from "../src/assets/contactheader.png"
+import {Link} from "react-router-dom";
 
 function Contactus() {
     return (
         <div className='h-screen w-screen'>
-            <div className='w-full h-[50%] bg-amber-500 bg-cover bg-center'
+            <div className='w-full h-[50%] bg-cover bg-center'
                  style={{backgroundImage: `url(${contactHeader})`}}>
             </div>
-            <div className='w-full h-[50%] bg-red-600'>
+            <div className='flex justify-center items-center flex-col  w-full h-[50%]'>
+                <br/>
+                <h1 className="text-5xl text-black">Contact Us</h1>
+                <br/>
+                <p className="text-xl font-bold">Email:<Link className="font-normal"
+                                                             to="mailto:xyz@gmail.com">xyz@gmail.com</Link></p>
+                <p className="text-xl font-bold">Contact No:<span className="font-normal">9999999999</span></p>
+                <br/>
+                <p className="text-xl ">**Note that we are available from 7:00AM to 7:00PM</p>
 
             </div>
         </div>
