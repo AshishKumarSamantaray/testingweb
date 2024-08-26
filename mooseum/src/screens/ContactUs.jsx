d6ae438662cb04f9d52d16680d306397eda6cd05
AshishKumarSamantaray <121350516+AshishKumarSamantaray@users.noreply.github.com>
2024-08-26 14:54:01 +0530

    commit

--- mooseum/src/screens/ContactUs.jsx
+++ mooseum/src/screens/ContactUs.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import contactHeader from "../../public/assets/contactheader.png"
+import {Link} from "react-router-dom";
 
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
+      <div className='h-screen w-screen'>
+        <div className='w-full h-[50%] bg-cover bg-center'
+             style={{backgroundImage: `url(${contactHeader})`}}>
+        </div>
+        <div className='flex justify-center items-center flex-col  w-full h-[50%]'>
+          <br/>
+          <h1 className="text-5xl text-black">Contact Us</h1>
+          <br/>
+          <p className="text-xl font-bold">Email:<Link className="font-normal"
+                                                       to="mailto:xyz@gmail.com">xyz@gmail.com</Link></p>
+          <p className="text-xl font-bold">Contact No:<span className="font-normal">9999999999</span></p>
+          <br/>
+          <p className="text-xl ">**Note that we are available from 7:00AM to 7:00PM</p>
+
+        </div>
+      </div>
   )
 }
 
