4b6c2eee3e49219e6c66f1af65ad12f0673f9340
AshishKumarSamantaray <121350516+AshishKumarSamantaray@users.noreply.github.com>
2024-08-26 17:22:11 +0530

    commit

--- mooseum/src/frontendComponents/HomeScreen.jsx
+++ mooseum/src/frontendComponents/HomeScreen.jsx
@@ -28,7 +28,7 @@ const HomeScreen = () => {
 
 
             <div className="ai absolute z-[10] bottom-[0%] w-[100%] h-[3.3vw] bg-amber-800 flex items-center justify-center">
-              <h1 className='text-[1vw] text-white font-thin'>To know more about the <span className='font-extrabold text-white'>AI</span>, <a href="" className='text-white underline'>Click Here</a></h1>
+              <h1 className='text-[1vw] text-white font-thin'>To know more about the <span className='font-bold text-white'>AI</span>, <a href="" className='text-white underline'>Click Here</a></h1>
             </div>
         </div>
     </>
