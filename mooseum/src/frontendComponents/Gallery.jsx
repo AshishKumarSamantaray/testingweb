285c97e3e13ffaa3bdc48b340ce3ac8e60cba229
Soham Ghosh <soham@Sohams-MacBook-Air.local>
2024-08-27 11:15:45 +0530

    hi

--- mooseum/src/frontendComponents/Gallery.jsx
+++ mooseum/src/frontendComponents/Gallery.jsx
@@ -41,7 +41,7 @@ const Gallery = () => {
   return (
     <>
       <LineRight component="GALLERY"/>
-      <div className="gallery__items w-[100%] max-h-[100vh] mt-[2vw] flex items-center justify-between ">
+      <div className="gallery__items w-[100%] h-[100vh] mt-[2vw] flex items-center justify-between ">
         {/* Part 1 */}
         <div className="part1 p-[2vw] w-[25%] h-[100%] flex gap-[2vw] flex-col">
           <div
