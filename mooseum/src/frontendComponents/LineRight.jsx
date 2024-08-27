bbacd047c9881194e09be3823877aeff2e8759f3
Soham Ghosh <soham@Sohams-MacBook-Air.local>
2024-08-27 12:18:52 +0530

    initial2

--- mooseum/src/frontendComponents/LineRight.jsx
+++ mooseum/src/frontendComponents/LineRight.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const LineRight = (props) => {
   return (
-    <div className="w-[100%] h-[2vw] mt-[2vw] p-[1vw] flex items-center justify-between">
-        <div className="line h-[0.1vw] mr-[2vw] ml-[2vw] w-[100%] bg-black"></div>
-        <h1 className='text-[2vw] pl-[1.8vw] h-[100%] mr-[2vw] max-w-[13vw] font-[400]'>{props.component}</h1>
+    <div className="w-[100%] h-[7vw] bg-red-900 p-[1vw] flex items-center justify-between">
+        <div className="line h-[0.1vw] mr-[3vw] ml-[2vw] w-[100%] bg-black"></div>
+        <div className='h-[100%] mr-[2vw] bg-green-900 w-[13vw] flex items-center justify-center'>
+          <h1 className='text-[2vw] font-[400]'>{props.component}</h1>
+        </div>
     </div>
   )
 }
